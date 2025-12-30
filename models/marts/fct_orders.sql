f5d91d7540bc70e00f5ae2579f5842eb2bee7f51
student <student@localhost.localdomain>
2025-12-30 18:34:28 +0530

    day12 add clean_string macro jinja loop dbt-utils and post-hook

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -3,6 +3,15 @@
     materialized='incremental',
     incremental_strategy='merge',
     unique_key='order_id',
+    on_schema_change='sync_all_columns',
+    post_hook=["GRANT SELECT ON {{ this }} TO ROLE OLIST_ROLE"]
+  )
+}}
+{{
+  config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='order_id',
     on_schema_change='sync_all_columns'
   )
 }}
